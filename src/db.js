65c4cdc8afa9cd88f3b6639aa6efb5bbc549a3d5
O'Brien <obrienadedapo15@gmail.com>
2024-09-04 11:54:58 +0100

    second commit

--- src/db.js
+++ src/db.js
@@ -11,7 +11,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "dapo",
   password: "oNHAZXM9WaSQ5qNCE7QIUEWOvz1wzvxB",
-  host: "dpg-crbnvbjv2p9s73dj4fng-a",
+  host: "postgresql://dapo:oNHAZXM9WaSQ5qNCE7QIUEWOvz1wzvxB@dpg-crbnvbjv2p9s73dj4fng-a.frankfurt-postgres.render.com/eproctor",
   port: "5432",
   database: "eproctor",
 });
